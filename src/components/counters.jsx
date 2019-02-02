cae57794a286cb08321285679e9127840ffba4ba
Sanjai <sanjaikumar4773@gmail.com>
2019-02-02 19:56:34 +0530

    initial commit

---
+++ src/components/counters.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+class Counters extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          onClick={this.props.onReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.props.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.props.onDelete} //bubiling the events like delete,increment to parent (App)
+            onIncrement={this.props.onIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          NavBar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
